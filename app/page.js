33578140d1624ab062ccd9840c8a3d5580465bff
JHNSTVN <164068054+JHNSTVN@users.noreply.github.com>
2024-06-05 11:41:07 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-2xl text-center p-5 my-4">CPRG 306: Web Development 2 - Assignments</h1>
+      <h1 className="text-2xl font-bold text-center p-5 my-4">CPRG 306: Web Development 2 - Assignments</h1>
       <div className="w-64">
         <Link href="/week-2">
           <button className="w-full h-12 bg-blue-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5">Week-2 Page 2</button>
