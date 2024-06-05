e1ec5a79ce6d772bcdc7cac167348280301e67b7
JHNSTVN <164068054+JHNSTVN@users.noreply.github.com>
2024-06-05 12:30:05 -0600

    Re-naming

--- app/page.js
+++ app/page.js
@@ -6,12 +6,17 @@ const Home = () => {
       <h1 className="text-2xl font-bold text-center p-5 my-4">CPRG 306: Web Development 2 - Assignments</h1>
       <div className="w-64">
         <Link href="/week-2">
-          <button className="w-full h-12 bg-blue-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5">Week-2 Page 2</button>
+          <button className="w-full h-12 bg-blue-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5">week-2</button>
         </Link>
       </div>
       <div className="w-64">
         <Link href="/week-3">
-          <button className="w-full h-12 bg-blue-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5">Week-3 Shopping list</button>
+          <button className="w-full h-12 bg-blue-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5">week-3</button>
+        </Link>
+      </div>
+      <div className="w-64">
+        <Link href="/week-4">
+          <button className="w-full h-12 bg-blue-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5">week-4</button>
         </Link>
       </div>
     </div>
