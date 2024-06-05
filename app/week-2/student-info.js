9880d726937bace0f90d0f3822415b95786b531b
JHNSTVN <164068054+JHNSTVN@users.noreply.github.com>
2024-06-04 18:04:25 -0600

    Week-3

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 const StudentInfo = () => {
   return (
-    <div>
-      <h1>Johan Nunez</h1>
-      <Link href="https://github.com/JHNSTVN/cprg306-assignments">Johan's Repo</Link>
+    <div className="flex flex-col items-center">
+      <h1 className="text-xl font-semibold p-5">Johan Nunez</h1>
+      <Link className="w-64 h-12 bg-blue-950 hover:bg-blue-900 text-white font-bold py-3 rounded text-center" href="https://github.com/JHNSTVN/cprg306-assignments">MY GITHUB</Link>
     </div>
   );
 };
